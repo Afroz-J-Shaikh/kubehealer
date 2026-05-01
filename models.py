cb1423335e7017be8468aec6a6b68c0352058f10
Afroz Shaikh <afsk1997@gmail.com>
2026-05-01 23:18:38 +0530

    Update models.py

--- models.py
+++ models.py
@@ -46,7 +46,7 @@ class ConversationInput:
 class OllamaRequest:
     messages: List[Dict[str, str]]
     system_prompt: str
-    model: str = "gemma2"
+    model: str = "gemma2:9b"
 
 @dataclass
 class OllamaResponse:
